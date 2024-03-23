976077d88045e7146b456c540d818e4abb8efa97
zsl8 <zezoalajwadi@gmail.com>
2024-03-23 18:17:04 +0300

    Add url package and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        sourcemap: false
+    }
 })
